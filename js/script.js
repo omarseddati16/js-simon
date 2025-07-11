7f3260a708720329a437fe03b05ee0cb76fd5b04
omarseddati16 <omarseddati16@gmail.com>
2025-07-11 15:58:33 +0200

    togliere i numeri

--- js/script.js
+++ js/script.js
@@ -6,8 +6,8 @@ const answersForm = document.getElementById('answers-form');
 const inputGroup = document.getElementById('input-group');
 const messageElement = document.getElementById('message');
 
-const numeriCasuali = Math.floor(Math.random() * 30) + 1;
 
+const inputElements = document.querySelectorAll('#input-group input[type="number"]');
 let count = 30;
 countdownElement.innerText = count;
 let timer = setInterval(function () {
@@ -16,11 +16,23 @@ let timer = setInterval(function () {
   if (count <= 0) {
     clearInterval(timer);
     countdownElement.innerText = 'Finito il tempo';
-  }
+
+  } else if (count <= 20)
+    for (let i = 0; i < inputElements.length; i++) {
+      inputElements[i].value = null
+    }
 }, 1000);
-document.getElementById('countdown').innerText = totalSecond;
+document.getElementById('countdown').innerText = count;
+
+
+const randomNumbers = []
+
+for (let i = 0; i < inputElements.length; i++) {
+  const randomNumber = Math.floor(Math.random() * 30) + 1;
+  inputElements[i].value = randomNumber;
+  randomNumbers.push(randomNumber)
+}
 
 
 
-
-
+button
